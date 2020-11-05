832e9cabb313177ec00caa99b36c0ddc44a8b73e
MarcosTavares <marcos.ricardo.tavares@gmail.com>
2020-11-05 11:12:56 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Migrations/20200703153451_TabelaProduto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class TabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(nullable: true),
+                    Quantidade = table.Column<int>(nullable: false),
+                    categoriaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_categoriaId",
+                        column: x => x.categoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_categoriaId",
+                table: "Produtos",
+                column: "categoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.TestExecutor;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+    {
+
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriasControllerTest()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+
+            _mockContext.Setup(m => m.Categorias).Returns(_mockSet.Object);
+
+            _mockContext.Setup(m => m.Categorias.FindAsync(1))
+                    .ReturnsAsync(_categoria);
+
+        }
+
+            [Fact]
+            public async Task Get_Categoria()
+            {
+                var service = new CategoriasController(_mockContext.Object);
+
+            //var testCategoria = await service.GetCategoria(1);
+            await service.GetCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1),
+                    Times.Once());
+
+
+            }
+        
+    }
+}
+
+
